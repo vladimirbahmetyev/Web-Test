380ffe3254d0a431c4e1c2fd63269ce1b745a0a5
Vladimir <vladimir240298perm@gmail.com>
2020-08-24 10:58:26 +0300

    Updates

---
+++ precache-manifest.d770a5cbbc069bf35c6a5dd630d17aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912d06281b1dd6757d0c9bb03297ddb9",
+    "url": "/Web-Test/index.html"
+  },
+  {
+    "revision": "419b7d8bd83fc6e2b03b",
+    "url": "/Web-Test/static/css/main.48e830d6.chunk.css"
+  },
+  {
+    "revision": "966cdb8cd8ac8ba9bd0d",
+    "url": "/Web-Test/static/js/2.6e813bed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Test/static/js/2.6e813bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419b7d8bd83fc6e2b03b",
+    "url": "/Web-Test/static/js/main.71ef7579.chunk.js"
+  },
+  {
+    "revision": "39f78e4c0b5a58421071",
+    "url": "/Web-Test/static/js/runtime-main.dc9e3716.js"
+  }
+]);
